66896148f3f797b576c159b1202216aed0a405b7
ahmad <ahmad.syukron6991@gmail.com>
2018-11-25 11:27:01 +0700

    upload dom

---
+++ index.js
@@ -0,0 +1,29 @@
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  
+    var slides = document.getElementsByClassName("slideBanner");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 5000);
+}
+
+function buttonFunc (){
+    let x, text;
+    x = document.getElementById("numb").value;
+
+    if ( x.length == 0) {
+        text = "mohon isi data dengan lengkap";
+    } 
+    document.getElementById("demo").innerHTML = text;
+}
